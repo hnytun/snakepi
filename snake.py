c008ceedd46a965fb81221e13628941b32a9e512
henrik <hnytun@gmail.com>
2023-10-09 22:58:14 +0200

    test

--- snake.py
+++ snake.py
@@ -2,25 +2,43 @@ from sense_hat import SenseHat
 from time import sleep
 
 sense = SenseHat()
-green = (0, 0, 255)
+green = (0, 255, 0)
 
-sense.clear()
-sense.set_pixel(2,2,green)
+
+
+pos = (2,2)
+sense.set_pixel(pos[0],pos[1],green)
+
+
+def move(direction,pos):
+    if(direction == "left"):
+        pos[0] -= 1
+    if(direction == "right"):
+        pos[0] += 1
+    if(direction == "up"):
+        pos[1] -= 1
+    if(direction == "down"):
+        pos[1] += 1
+
+    return pos
 
 
 while(True):
-    sleep(1)
+    #sleep(1)
     for event in sense.stick.get_events():
         # Check if the joystick was pressed
         if event.action == "pressed":
             if(event.direction == "left"):
-                print("left!")
+                pos = move(pos,"left")
             if(event.direction == "right"):
-                print("right!")
+                pos = move(pos,"right")
             if(event.direction == "up"):
-                print("up!")
+                pos = move(pos,"up")
             if(event.direction == "down"):
-                print("down!")
+                pos = move(pos,"down")
+            sense.clear()
+            sense.set_pixel(pos[0],pos[1],green)
+
 
 
 
