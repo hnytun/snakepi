9cfc2b58163458719ca6effce4a53c276114a648
henrik <hnytun@gmail.com>
2023-10-09 22:48:34 +0200

    test

--- snake.py
+++ snake.py
@@ -7,4 +7,19 @@ sense.clear()
 sense.set_pixel(2,2,green)
 
 
+while(True):
+    sleep(1)
+    for event in sense.stick.get_events():
+        # Check if the joystick was pressed
+        if event.action == "pressed":
+            if(event.direction == "left"):
+                print("left!")
+            if(event.direction == "right"):
+                print("right!")
+            if(event.direction == "up"):
+                print("up!")
+            if(event.direction == "down"):
+                print("down!")
+
+
 
