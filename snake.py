db486791f3519e6e75b1efe82299fc7d313075a1
henrik <hnytun@gmail.com>
2023-10-10 00:21:21 +0200

    test

--- snake.py
+++ snake.py
@@ -5,11 +5,11 @@ sense = SenseHat()
 sense.clear()
 green = (0, 255, 0)
 
-
-body = [[5,3],[4,3]]
+#        <~~~~~~~~--         
+body = [[4,3],[3,3]]
 sense.set_pixel(body[0][0],body[0][1],green)
 sense.set_pixel(body[1][0],body[1][1],green)
-sense.set_pixel(body[2][0],body[2][1],green)
+
 
 
 def move(body,direction):
