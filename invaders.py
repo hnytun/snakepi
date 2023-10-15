d1be9d72b4b7f6e04566a671d9afb380299bef38
henrik <hnytun@gmail.com>
2023-10-15 11:37:24 +0200

    game

--- invaders.py
+++ invaders.py
@@ -71,7 +71,7 @@ while(True):
             if(event.direction == "right" and ship.x < 7):
                 ship.move("right")
             if(event.direction == "up"):
-                projectile = threading.Thread(target=projectile_task, name='projectile', args=(ship.x))
+                projectile = threading.Thread(target=projectile_task, name='projectile', args=(ship.x,))
                 projectile.start()
 
 
